d776d957efb176d98b8e7411de39fc09bb333b1b
Fetivol <Fetisowova@gmail.com>
2023-11-14 22:20:29 +0100

    ES6 moduls

---
+++ routes/api/contacts-router.js
@@ -0,0 +1,29 @@
+// const express = require('express')
+import express from "express";
+
+import contactServise from "../../models/contacts/index.js";
+
+const contactsRouter = express.Router();
+
+contactsRouter.get("/", async (req, res, next) => {
+  const result = await contactServise.listContacts;
+  res.json(result);
+});
+
+contactsRouter.get("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
+
+contactsRouter.post("/", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
+
+contactsRouter.delete("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
+
+contactsRouter.put("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
+
+export default contactsRouter;
