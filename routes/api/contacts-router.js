286ba169d8e77ed3e361c8dea31e21232626b8f4
Fetivol <Fetisowova@gmail.com>
2023-11-21 19:13:51 +0100

    decorators

--- routes/api/contacts-router.js
+++ routes/api/contacts-router.js
@@ -4,16 +4,33 @@ import contactsControllers from "../../controllers/contacts-controllers.js";
 
 import { isEmptyBody } from "../../middlewares/index.js";
 
+import { validateBody } from "../../decorators/index.js";
+
+import {
+  contactAddSchema,
+  contactUpdateSchema,
+} from "../../schemas/contacts-schemas.js";
+
 const contactsRouter = express.Router();
 
 contactsRouter.get("/", contactsControllers.getAll);
 
 contactsRouter.get("/:contactId", contactsControllers.getById);
 
-contactsRouter.post("/", isEmptyBody, contactsControllers.add);
+contactsRouter.post(
+  "/",
+  isEmptyBody,
+  validateBody(contactAddSchema),
+  contactsControllers.add
+);
 
 contactsRouter.delete("/:contactId", contactsControllers.deleteById);
 
-contactsRouter.put("/:contactId", isEmptyBody, contactsControllers.updateById);
+contactsRouter.put(
+  "/:contactId",
+  isEmptyBody,
+  validateBody(contactUpdateSchema),
+  contactsControllers.updateById
+);
 
 export default contactsRouter;
