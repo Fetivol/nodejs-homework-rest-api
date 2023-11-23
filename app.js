33ae7b33435736c7e972e6660068c489490ac5e8
Fetivol <Fetisowova@gmail.com>
2023-11-23 21:19:42 +0100

    add DB to the project

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 import express from "express";
 import logger from "morgan";
 import cors from "cors";
+import dotenv from "dotenv";
+import "dotenv/config";
 
 import contactsRouter from "./routes/api/contacts-router.js";
 
+dotenv.config();
 const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
--- server.js
+++ server.js
@@ -1,6 +1,18 @@
+import mongoose from "mongoose";
+
 import app from "./app.js";
-// Volodymyr
-// h4lWnIzXzzoTLVGl
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+
+// import { DB_HOST } from "./config.js";
+const { DB_HOST } = process.env;
+mongoose
+  .connect(DB_HOST, (PORT = 3000))
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`);
+    });
+    console.log("Database connection successful");
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
