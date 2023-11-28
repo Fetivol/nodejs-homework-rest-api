9aaaa6e397a6be14c2530eba3c84061ab0009a02
Fetivol <Fetisowova@gmail.com>
2023-11-28 21:24:39 +0100

    Update ctrlWrapper.js

--- decorators/ctrlWrapper.js
+++ decorators/ctrlWrapper.js
@@ -1,10 +1,11 @@
-const func = async (req, res, next) => {
-  try {
-    await ctrl(req, res, next);
-  } catch (error) {
-    next(error);
-  }
+const ctrlWrapper = (ctrl) => {
+  return async (req, res, next) => {
+    try {
+      await ctrl(req, res, next);
+    } catch (error) {
+      next(error);
+    }
+  };
 };
-return func;
 
 export default ctrlWrapper;
